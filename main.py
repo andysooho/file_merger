10eca32c3b635a0e93e8235dc558b04c8780074a
Andrew <andysooho@gmail.com>
2023-11-15 18:46:35 +0900

    init: 저장되는 파일 이름에 날짜랑 시간 추가
    저장형식
    result_20231115_184448.txt

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog, ttk
 import os
+from datetime import datetime
 
-# 기본 윈도우 생성
+# 기본 윈도우
 root = tk.Tk()
 root.title("파일 병합기")
 
@@ -10,6 +11,7 @@ root.title("파일 병합기")
 # 경로 입력 필드
 path_entry = tk.Entry(root, width=50)
 path_entry.pack()
+
 
 # 경로 선택 버튼
 def browse_path():
@@ -36,6 +38,7 @@ scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 # 트리뷰와 스크롤바 연동
 tree.configure(yscrollcommand=scrollbar.set)
 
+
 def load_tree(path):
     for i in tree.get_children():
         tree.delete(i)
@@ -43,19 +46,25 @@ def load_tree(path):
         for file in files:
             tree.insert('', 'end', text=os.path.join(root, file))
 
+
 # 트리뷰에 체크박스 추가
 def add_checkboxes():
     pass  # 체크박스 추가 로직 구현
 
+
 def merge_files():
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"result_{current_time}.txt"
+
     selected_items = tree.selection()
-    with open("result.txt", "w", encoding="utf-8") as result_file:
+    with open(filename, "w", encoding="utf-8") as result_file:
         for item in selected_items:
             path = tree.item(item, 'text')
             result_file.write(path + "\n")
             with open(path, "r", encoding="utf-8") as file:
                 content = file.read()
                 result_file.write(content + "\n\n")
+
 
 merge_button = tk.Button(root, text="파일 병합", command=merge_files)
 merge_button.pack()
